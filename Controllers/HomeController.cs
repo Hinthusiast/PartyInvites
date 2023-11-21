96a2cfa71ebb79fecc2140586b3f211990b7f406
Ciro Brigade <ricrobri@gmail.com>
2023-11-21 13:44:29 +0800

    Created Domain Model - GuestResponse class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ namespace PartyInvites.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+ /*   private readonly ILogger<HomeController> _logger;
 
-/*    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }*/
